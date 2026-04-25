70866cc22a8b08f61ee286a91de9d763f8fedf6c
Tiorenn <ericorenanleal124@gmail.com>
2026-04-25 20:56:36 -0300

    Connectando ao mongoDB com repository e service

--- src/main/java/com/ericorenan/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ericorenan/workshopmongo/resources/UserResource.java
@@ -1,28 +1,25 @@
 package com.ericorenan.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ericorenan.workshopmongo.domain.User;
+import com.ericorenan.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
     
+    @Autowired
+    private UserService service;
+    
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        
-        User maria = new User("12", "Maria brown", "Maria@gmail.com");
-        User alex = new User("13", "Alex green", "AlexGreen@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria,alex));
-
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/ericorenan/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.ericorenan.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericorenan.workshopmongo.domain.User;
+import com.ericorenan.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
