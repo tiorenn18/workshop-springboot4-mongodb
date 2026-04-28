8fde327a3ab85126bf361b0ff6203cd14edba79b
Tiorenn <ericorenanleal124@gmail.com>
2026-04-28 02:07:06 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/ericorenan/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ericorenan/workshopmongo/resources/UserResource.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.http.ResponseEntity;
 
 import com.ericorenan.workshopmongo.domain.User;
 import com.ericorenan.workshopmongo.dto.UserDTO;
@@ -49,4 +50,9 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> deleteById(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
